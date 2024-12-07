01d05014b2c4a6ee96985a38d17508314dfaf5f7
Dharani204 <bdharani164@gmail.com>
2024-12-07 11:49:27 +0530

    Add files via upload

--- convert emoji to text.py
+++ convert emoji to text.py
@@ -29,29 +29,37 @@ emoji_dict ={
 }
 #function to start the emoji to text game
 def emoji_game():
+    #Introduction to the game
     print("Welcome to the 'convert the Emoji to Text' Game!")
     print("In this game, you will be shown an emoji.Type the correct description to earn points.")
     print("Type 'quit' to exit the game.")
     print("-"*40)
 
     score=0 #keep track of the player's score
-    emojis=list(emoji_dict.keys())
+    emojis=list(emoji_dict.keys()) #Extract all emoji keys from the dictionary
 
+    #Infinite loop to keep the game running until the player chooses to quit
     while True:
-        emoji=random.choice(emojis)
+        emoji=random.choice(emojis) #Randomly selects an emoji from the list
         print(f"Emoji: {emoji}")
+
+        #Prompts the user for their answer and standerdizes it from comparison
         answer=input("Your Answer:").strip().lower()
 
+        #Exist the game if the user types 'quit'
         if answer=="quit":
             print(f"Game Over! Your Final Score: {score}")
             break
 
+        #Checks if the answer is correct
         if answer==emoji_dict[emoji]:
-            print("Correct!🎉")
-            score+=1
+            print("Correct!🎉") #Print the correct answer
+            score+=1 #Increments the score
         else:
+            #Displays the correct answer if the user's input is wrong
             print(f"Wrong! The correct answer is: '{emoji_dict[emoji]}'")
 
+        #Displays the player's current score
         print(f"Your Current Score: {score}") 
         print("-"*40)
     
